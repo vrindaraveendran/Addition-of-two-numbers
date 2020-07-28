75ea18baf5d674919a2786f88e6bec695fdfe611
vrindaraveendran <48712834+vrindaraveendran@users.noreply.github.com>
2020-07-28 17:53:30 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,sum=0;
+	printf("Enter 1st number: \n");
+	scanf("%d",&a);
+	printf("Enter 2nd number: \n");
+	scanf("%d",&b);
+	sum=a+b;
+    printf("Sum=%d",sum);
+}
